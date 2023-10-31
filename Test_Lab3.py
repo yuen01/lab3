55e3a46dda5f28a117cf8d549ccc2c6c2183479d
joseph <yuen01.22@ichat.sp.edu.sg>
2023-10-31 16:31:09 +0800

    lab3

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -2,7 +2,6 @@ import Lab3
 
 print("Test_Lab3")
 
-
 def test_bubble_sort_ascending():
     result = []
     input_arr = [64, 34, 25, 12, 22, 11, 90]
@@ -28,3 +27,49 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_large_input():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 5, 8, 7, 2]
+    result = Lab3.bubble_sort(input_arr, "SORT_ASCENDING")
+    assert result == 1
+
+def check_integers_in_array(arr):
+    count = 0
+    for item in arr:
+        if isinstance(item, int):
+            count += 1
+    if count >= 10:
+        return 1
+    else:
+        return 0
+
+def test_check_integers_in_array():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 5, 8, 7, 2]
+    result = check_integers_in_array(input_arr)
+    print(result)
+
+def check_integers_in_array2(arr):
+    count = 0
+    for item in arr:
+        if isinstance(item, int):
+            count += 1
+    if count == 0:
+        return 0
+    else:
+        return 1
+
+def test_check_integers_in_array2():
+    input_arr = []
+    result = check_integers_in_array2(input_arr)
+    print(result)
+
+
+def check_integers_in_array3(arr):
+    for item in arr:
+        if not isinstance(item, int):
+            return 2
+    return 1
+def test_check_integers_in_array3():
+    input_arr = [64, 34, 25, 12, 22, 11,"test", 5, 8, 7, 2]
+    result = check_integers_in_array3(input_arr)
+    print(result)
